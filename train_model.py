237c99c0f4988ef2c2c719f063d967af5f8673e1
L4rfin <122998012+L4rfin@users.noreply.github.com>
2025-06-13 12:15:44 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -5,15 +5,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-# Load data from the text file
 data_file = "data.txt"
 data = np.loadtxt(data_file)
 
-# Split data into features (X) and labels (y)
-X = data[:, :-1]  # Features are all columns except the last one
-y = data[:, -1]   # Labels are the last column
 
-# Split the data into training and testing sets
+X = data[:, :-1]  
+y = data[:, -1]   
+
 X_train, X_test, y_train, y_test = train_test_split(X,
                                                     y,
                                                     test_size=0.2,
@@ -21,16 +19,12 @@ X_train, X_test, y_train, y_test = train_test_split(X,
                                                     shuffle=True,
                                                     stratify=y)
 
-# Initialize the Random Forest Classifier
 rf_classifier = RandomForestClassifier()
 
-# Train the classifier on the training data
 rf_classifier.fit(X_train, y_train)
 
-# Make predictions on the test data
 y_pred = rf_classifier.predict(X_test)
 
-# Evaluate the accuracy of the model
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy * 100:.2f}%")
 print(confusion_matrix(y_test, y_pred))
